bb901de1532be5cd7a8230e26ce0cd295f1e4a3c
Tim Wendisch <info@wendisch-media.de>
2019-04-01 23:14:43 +0200

    initial upload

---
+++ ext_emconf.php
@@ -0,0 +1,45 @@
+<?php
+
+/* * *************************************************************
+ * Extension Manager/Repository config file for ext "phpOfficeWord".
+ *
+ * Auto generated 01-04-2019 22:12
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ * ************************************************************* */
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Library phpOfficeWord',
+    'description' => 'Just a wrapper for the famous library. The original sources taken from https://github.com/PHPOffice',
+    'category' => 'misc',
+    'shy' => 0,
+    'version' => '0.16.0',
+    'dependencies' => 'cms',
+    'conflicts' => '',
+    'priority' => '',
+    'loadOrder' => '',
+    'module' => '',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearcacheonload' => 0,
+    'lockType' => '',
+    'author' => 'Mark Baker, Gabriel Bull, Franck Lefevre, Ivan Lanin, Roman Syroeshko, Antoine de Troostembergh, Tim Wendisch',
+    'CGLcompliance' => '',
+    'CGLcompliance_note' => '',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '8.7.0-8.7.99',
+            'php' => '5.6.0-7.2.99',
+            'phpOfficeCommon' => '0.2.9'
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+    'suggests' => array(
+    ),
+);
